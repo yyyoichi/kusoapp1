0f67688b77a65c455629e1676aa62c0c828ff98d
yyyoichi <origamiichiro@gmail.com>
2022-07-03 20:31:14 +0900

    add extramationmark

--- pages/coution.tsx
+++ pages/coution.tsx
@@ -3,7 +3,7 @@ import { saveAs } from 'file-saver';
 import GIFEncoder from "gif-encoder-2";
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image';
+import {default as NextImage } from 'next/image';
 import { useRouter } from 'next/router'
 import { useCallback, useEffect, useRef, useState } from 'react'
 import styles from '../styles/Home.module.css'
@@ -13,8 +13,6 @@ const HEIGHT = 300;
 const Home: NextPage = () => {
   const router = useRouter()
   const [value, setValue] = useState<string>("危険！")
-
-  // const clickMain = () => setState(s => { return { ...s, open: true } })
   const onChange = (v: string) => {
     setValue(v)
     router.push({
@@ -23,41 +21,27 @@ const Home: NextPage = () => {
     })
   }
   const canvasRef = useRef(null);
-  const [loading, setLoading] = useState<boolean>(false);
-  const download = useCallback(() => {
-    console.log("create");
+  useEffect(() => {
+    const image = new Image()
+    image.src = "/image/coution.png"
     const canvas: any = canvasRef.current;
     const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-    const encoder = new GIFEncoder(WIDTH, HEIGHT);
-    const ratio = [0, 6, 6, 6, 6, 4, 2, -2, -4, -6, -6, -6, -6/**0 */, -6, -6, -6, -6, -4, -2, 4, 6, 6, 6, 6];
-    const delay = 2000 / ratio.length;
-    encoder.setDelay(delay);
-    encoder.start();
-    ctx.font = "250px serif"
-    ctx.textBaseline = "top"
-    ctx.textAlign = "left"
-
-    for (const r of ratio) {
-      ctx.fillStyle = "white";
-      ctx.fillRect(0, 0, WIDTH, HEIGHT);
-      ctx.fillText(value, 0, 16);
-      encoder.addFrame(ctx);
-      ctx.translate(WIDTH / 2, HEIGHT / 2);
-      ctx.rotate(r * Math.PI / 180);
-      ctx.translate(-WIDTH / 2, -HEIGHT / 2);
-      ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    image.onload = () => {
+      ctx.drawImage(image, 0, 10, 50, 50)
+      ctx.drawImage(image, 50, 10, 50, 50)
+      ctx.drawImage(image, 100, 10, 50, 50)
+      ctx.drawImage(image, 150, 10, 50, 50)
+      ctx.drawImage(image, 200, 10, 50, 50)
+      ctx.drawImage(image, 250, 10, 50, 50)
+      ctx.drawImage(image, 0, 240, 50, 50)
+      ctx.drawImage(image, 50, 240, 50, 50)
+      ctx.drawImage(image, 100, 240, 50, 50)
+      ctx.drawImage(image, 150, 240, 50, 50)
+      ctx.drawImage(image, 200, 240, 50, 50)
+      ctx.drawImage(image, 250, 240, 50, 50)
     }
-    encoder.finish();
-    const buffer = encoder.out.getData();
-    const blob = new Blob([buffer], { type: "image/gif" });
-    saveAs(blob, `${value}.gif`);
-    console.log("finish");
-    setLoading(false);
-  }, [value]);
-  useEffect(() => {
-    if (!loading) return;
-    setTimeout(download, 500);
-  }, [loading, download]);
+  }, [])
+  const [loading, setLoading] = useState<boolean>(false);
   return (
     <div className={styles.wrapper}>
       <Head>
@@ -69,11 +53,11 @@ const Home: NextPage = () => {
       </Head>
       <div className={loading ? styles.loadingBox : styles.downloadBox} onClick={() => setLoading(true)} >
         {
-          loading ? <Image
+          loading ? <NextImage
             src={"/image/loading.png"}
             alt="ローディング"
             width={50}
-            height={50} /> : <Image
+            height={50} /> : <NextImage
             src={"/image/download.png"}
             alt={"ダウンロード"}
             width={50}
@@ -81,14 +65,13 @@ const Home: NextPage = () => {
           />
         }
       </div>
-      <div>
+      {/* <div> */}
         <input type="text" value={value} style={{fontSize: "24px"}} onChange={e => onChange(e.target.value)}/>
         <canvas
-          // style={{ display: "none" }}
           width={WIDTH}
           height={HEIGHT}
-          ref={canvasRef}></canvas>
-      </div>
+          ref={canvasRef} />
+      {/* </div> */}
     </div >
   )
 }
